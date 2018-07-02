ec3a56ffc7a6a46b799ab3cfcdd9e1bb7d530181
metherton <martinetherton63@gmail.com>
2018-07-02 20:54:48 +0200

    second commit

---
+++ src/main/java/com/martinetherton/EthertonportalserverApplication.java
@@ -0,0 +1,12 @@
+package com.martinetherton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthertonportalserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthertonportalserverApplication.class, args);
+	}
+}
