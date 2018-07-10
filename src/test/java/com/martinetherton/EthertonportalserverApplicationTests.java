1eda775bc04e144021c9a0f14cba75adc7fab2cf
metherton <martinetherton63@gmail.com>
2018-07-10 23:46:49 +0200

    ignore test

--- src/test/java/com/martinetherton/EthertonportalserverApplicationTests.java
+++ src/test/java/com/martinetherton/EthertonportalserverApplicationTests.java
@@ -1,5 +1,6 @@
 package com.martinetherton;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,14 +10,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class EthertonportalserverApplicationTests {
 
-//	@Test
-//	public void contextLoads() {
-//	}
-
 	@Test
-	public void testDummy() {
-		assert true;
+	@Ignore
+	public void contextLoads() {
 	}
+
+
 
 
 }
